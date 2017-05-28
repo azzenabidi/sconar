b66d7edd39e6515dd168ca24a24f6e46fafa58d3
azzenovic <azzen.abidi@gmail.com>
2017-05-28 02:08:37 +0100

    heroku keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,22 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
+
+if Rails.env.production?
   config.fog_credentials = {
-  provider:              'AWS',            
-  aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],          
-  aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],             
-  region:                ENV['ARTIFACTS_REGION']            
-}
-    config.fog_directory  = 'socify' # required
-    config.fog_public     = true # optional, defaults to true
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
-  end
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['AWS_SECRET_KEY']
+  }
+  config.fog_directory     =  ENV['S3_BUCKET_NAME']
+  config.storage           =  :fog
+else
+  config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => '',
+      :aws_secret_access_key  => '',
+      :region                 => 'eu-west-1'
+  }
+  config.storage = :file
+  config.cache_dir = "#{Rails.root}/public/tmp"
+end
 end
